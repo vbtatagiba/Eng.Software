8db36a2c8e3a83706eb6ae2abfe7c8c00491d817
vbtatagiba <102767374+vbtatagiba@users.noreply.github.com>
2022-04-08 18:41:45 -0300

    aula da chuva

---
+++ Pensamento Computacional/Aula 2/Victor/media.py
@@ -0,0 +1,10 @@
+#media
+n1=int(input('Digite o primeiro nota:'))
+n2=int(input('Digite o segundo nota:'))
+n3=int(input('Digite o terceiro nota:'))
+soma=(n1+n2+n3)/3
+if (soma<=4):
+    print ("reprovado")
+else:
+    print ("aprovado")
+
