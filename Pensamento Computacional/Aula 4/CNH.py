6e87c43c158a6e2b4847e4fedc9b2f6d0c59867c
vbtatagiba <102767374+vbtatagiba@users.noreply.github.com>
2022-04-08 21:57:52 -0300

    Add files via upload

---
+++ Pensamento Computacional/Aula 4/CNH.py
@@ -0,0 +1,5 @@
+idade=int(input("informe sua Idade"))
+if idade >=18 and idade<=80:
+    print('OK você pode dirigir')
+if idade <=17:
+    print("opa você não pode dirigir")
---
+++ Pensamento Computacional/Aula 4/calculadora.py
@@ -0,0 +1,29 @@
+# CALCULADORA
+n1 = int(input('\nDigite o PRIMEIRO NÚMERO: '))
+n2 = int(input('Digite o SEGUNDO NÚMERO: '))
+n3 = int(input('Digite o TERCEIRO NÚMERO: '))
+n4 = int(input('Digite o QUARTO NÚMERO: '))
+operador=str(input('qual o operador'))
+
+soma = n1+n2+n3+n4
+sub = n1-n2
+mult = n1*n2
+div = n1/n2
+divi_int = n1//n2
+expo = n1**n2
+rest = n1%n2
+
+if (operador=="+"):
+    print(f'\nA Soma de {n1} e {n2} é igual a: {soma}')
+elif (operador=="-"):
+    print(f'A Subtração de {n1} e {n2} é igual a: {sub}')
+elif (operador=="*"):
+    print(f'A Multiplicação de {n1} e {n2} é igual a: {mult}')
+elif (operador=="/"):
+    print(f'A Divisão de {n1} e {n2} é igual a: {div}')
+elif (operador=="//"):
+    print(f'A Divisão Inteira de {n1} e {n2} é igual a: {div}')
+elif (operador=="**"):
+    print(f'O Exponencial de {n1} para {n2} é igual a: {expo}')
+else:
+    print(f'O Resto da divisão entre {n1} e {n2} é igual a: {rest}\n')
