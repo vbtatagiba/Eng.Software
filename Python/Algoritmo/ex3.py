f6120c03bedb28786e77a0535e0021e4111345f8
vbtatagiba <102767374+vbtatagiba@users.noreply.github.com>
2022-10-18 21:09:39 -0300

    Create ex3.py

--- Python/Algoritmo/ex3.py
+++ Python/Algoritmo/ex3.py
@@ -1,4 +1,4 @@
 '''
 Faça um programa que receba uma lista com 10 numeros e uma segunda lista com 5 numeros.
-mostre os numeros da primeira lista com os seus respectivos divisores da 2 lista bem como suas posições 
+mostre os numeros da primeira lista com os seus respectivos divisores da 2 lista bem como suas posições use mod '%' 
 '''
