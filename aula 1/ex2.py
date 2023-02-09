9fddee4f94189178a7e90de47d88cd12d4a4f914
vbtatagiba <102767374+vbtatagiba@users.noreply.github.com>
2023-02-09 20:54:13 -0300

    aula orientada

---
+++ aula 1/ex2.py
@@ -0,0 +1,8 @@
+nomes=["Victor" , "Fábio","Paulo"]
+# nomes.sort()
+# if nomes[0] > nomes[2] > nomes[1] :
+#     print (nomes[1],nomes[2],nomes[0])
+# print (nomes[1])
+# for key in nomes:
+#     print (nomes)
+print()
