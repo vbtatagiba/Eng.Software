a85e938a287e06a13879db5d4d36e87dc7d84db2
vbtatagiba <102767374+vbtatagiba@users.noreply.github.com>
2023-03-09 21:48:19 -0300

    aula 4

---
+++ Estutura de dados/Aula 4/ex1.py
@@ -0,0 +1,14 @@
+'''Dado o conjunto de elementos abaixo, criar um algoritmo que seja capaz de encontrar o somatório absoluto do vetor.
+
+'''
+import math
+conjunto=[2/9,9,0,-5,10,15]
+conjunto1=2/3**2
+conjunto2=math.sqrt(81)
+conjunto3=3**-1
+conjunto3=-5
+conjunto4=10
+conjunto5=15
+
+resultado=conjunto1+conjunto2+conjunto3+conjunto4+conjunto5
+print('O Resultado do conjunto é',resultado)
