49d95a826e8e3f0132ec8ea101de1767ba93f7e6
Victor Bernardo Gomes de Tatagiba <102767374+vbtatagiba@users.noreply.github.com>
2023-05-04 20:56:48 -0300

    Aula 9 Criação de um Caixa 24h Fluxo
    Proxima Aula fazer caixa em py

---
+++ Estutura de dados/Aula 9/caixa.py
@@ -0,0 +1,9 @@
+import datetime
+data=datetime
+contas={'Cartão'(202211960),'Senha','2220'+data}
+for contas in contas:   
+    if contas == contas:
+        print(contas)
+# def verifica():
+#     ini=input('Seja Bem Vindo ao Caixa 24h por Favor Insira seu Cartão.:')
+#     if ini==contas(2):
